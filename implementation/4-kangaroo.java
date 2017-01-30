dd79a4ba9f5e9f5b49f4c8c09520a41034320984
jhonnysanchezillisaca <jhonny15993@gmail.com>
2017-01-30 22:54:25 +0100

    Started exercise 4 of implementation section

---
+++ implementation/4-kangaroo.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x1 = in.nextInt();
+        int v1 = in.nextInt();
+        int x2 = in.nextInt();
+        int v2 = in.nextInt();
+    }
+}
